2ad7ff37189c2d8b60fe52a5a9a4e34976fc14ab
Mryyy5200 <1677367467@qq.com>
2020-08-07 00:30:15 +0800

    08-06 3

--- app/src/main/java/com/edu/edusystem/activity/AnswerQuestionsActivity.java
+++ app/src/main/java/com/edu/edusystem/activity/AnswerQuestionsActivity.java
@@ -21,14 +21,6 @@ public class AnswerQuestionsActivity extends AppCompatActivity {
         answer_questions_WebView = findViewById(R.id.answer_questions_WebView);
         answer_questions_MaterialToolbar = findViewById(R.id.answer_questions_MaterialToolbar);
 
-        //WebSettings webSettings = answer_questions_WebView.getSettings();
-        //webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
-        //webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
-
-        //webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
-        //webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
-        //webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
-
         //answer_questions
         answer_questions_WebView.loadUrl("file:///android_asset/answer_questions.html");
 
